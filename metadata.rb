0cb19388e7c38ae5dfd9c4c0a0d679b27c34157d
Mathieu Sauve-Frankel <msf@kisoku.net>
2016-01-26 18:57:22 +0900

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'msf@kisoku.net'
 license 'apache2'
 description 'Installs/Configures baryon'
 long_description 'Installs/Configures baryon'
-version '0.1.0'
+version '0.1.1'
 
 issues_url 'https://github.com/kisoku/baryon-cookbook/issues'
 source_url 'https://github.com/kisoku/baryon-cookbook'
